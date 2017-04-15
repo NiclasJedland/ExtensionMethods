4cb98440c40bf5b8b951d4122afc8c814def121f
Niclas Jedland <jedland82@hotmail.com>
2017-04-15 19:47:36 +0200

    Add project files.

---
+++ ExtensionMethod/ExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+	public static class ExtensionMethods
+	{
+		public static double ToDouble(this string input)
+		{
+			if (double.TryParse(input, out double output))
+				return output;
+			else
+				throw new Exception();
+		}
+
+		public static string GetOnlyNumbersFromString(this string input)
+		{
+			if (string.IsNullOrEmpty(input))
+				return "";
+			return new String(input.Where(Char.IsDigit).ToArray());
+		}
+
+		public static bool IsBetweenDate(this DateTime inputDate, DateTime fromDate, DateTime toDate)
+		{
+			return inputDate.Date >= fromDate.Date && inputDate.Date <= toDate.Date;
+		}
+	}
+}
